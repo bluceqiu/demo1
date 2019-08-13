7b16e1e1ce217c282ecf4a3b43e6aeb24e5067ca
420343878@qq.com <420343878@qq.com>
2019-08-13 19:42:57 +0800

    save

---
+++ doc/render.js
@@ -0,0 +1,10 @@
+function render( createElement ) { // createElement 是个函数， 
+    
+    
+    return VNode; // 也就是我们要渲染的节点
+}
+
+function createElement( ){
+
+    return VNode;
+}
---
+++ doc/trybabel.js
@@ -0,0 +1,1 @@
+const  arr = [1,2,3].map((item, index)=>item*2);
